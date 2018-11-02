fd2a79f149034bf6131c32b0071bf22a56e16bbf
Vikkkkk <vicguo.99@gmail.com>
2018-11-02 10:47:20 -0700

    ball return when hit border

--- src/partials/Ball.js
+++ src/partials/Ball.js
@@ -10,6 +10,8 @@ export default class Ball {
   }
 
   reset() {
+    // this.ax = 0.001;
+    // this.ay = 0.001;
     this.x = this.boardWidth / 2;
     this.y = this.boardHeight / 2;
 
@@ -20,10 +22,27 @@ export default class Ball {
     this.vx = this.direction * (6 - Math.abs(this.vy));
   }
 
+  wallCollision() {
+    const hitLeft = this.x - this.radius <= 0;
+    const hitRight = this.x + this.radius >= this.boardWidth;
+    const hitTop = this.y - this.radius <= 0;
+    const hitBottom = this.y + this.radius >= this.boardHeight;
+
+    if (hitRight || hitLeft) {
+      this.vx *= -1;
+    } else if (hitTop || hitBottom) {
+      this.vy *= -1;
+    }
+  }
+
   render(svg) {
+    // this.vx += this.ax;
+    // this.vy += this.ay;
+
     this.x += this.vx;
     this.y += this.vy;
 
+    this.wallCollision();
     const ball = document.createElementNS(SVG_NS, 'circle');
     ball.setAttributeNS(null, 'cx', this.x);
     ball.setAttributeNS(null, 'cy', this.y);
