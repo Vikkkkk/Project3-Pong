ee224921fdaed344f91fe92da36d07efac418c88
Vikkkkk <vicguo.99@gmail.com>
2018-11-02 09:44:24 -0700

    moving balls

--- src/partials/Ball.js
+++ src/partials/Ball.js
@@ -6,12 +6,27 @@ export default class Ball {
     this.boardHeight = boardHeight;
     this.color = color;
     this.direction = 1;
+    this.reset();
+  }
+
+  reset() {
+    this.x = this.boardWidth / 2;
+    this.y = this.boardHeight / 2;
+
+    this.vy = 0;
+    while (this.vy === 0) {
+      this.vy = Math.floor(Math.random() * 10 - 5);
+    }
+    this.vx = this.direction * (6 - Math.abs(this.vy));
   }
 
   render(svg) {
+    this.x += this.vx;
+    this.y += this.vy;
+
     const ball = document.createElementNS(SVG_NS, 'circle');
-    ball.setAttributeNS(null, 'cx', this.boardWidth / 2);
-    ball.setAttributeNS(null, 'cy', this.boardHeight / 2);
+    ball.setAttributeNS(null, 'cx', this.x);
+    ball.setAttributeNS(null, 'cy', this.y);
     ball.setAttributeNS(null, 'r', this.radius);
     ball.setAttributeNS(null, 'fill', this.color);
     svg.appendChild(ball);
